61e6de006ef0b19068ce9ec71e6c9537a303278b
Shuji-Watanabe <shuji-watanabe@neptune.kanazawa-it.ac.jp>
2025-01-29 18:47:03 +0900

    ver20250129-004

--- pages/1_線形回帰.py
+++ pages/1_線形回帰.py
@@ -17,31 +17,34 @@ st.subheader("データ分析",divider="orange")
 
 
 display_col02 = st.columns([1,1])
-with display_col02[1]:
+with display_col02[0]:
+    if st.button("初期化") or ("history" not in st.session_state):
+        st.session_state.history = []  # 初期化 
     # p, q のスライダー設定
-    p = st.number_input("傾き p", min_value=-5.0, max_value=5.0, value=1.0, step=0.1)
-    q = st.number_input("切片 q", min_value=-5.0, max_value=5.0, value=0.0, step=0.1)
+    num_step = st.number_input("探索ステップ", value=0.01)
+    p = st.number_input("傾き p", min_value=-5.00, max_value=5.00, value=1.00, step=num_step)
+    q = st.number_input("切片 q", min_value=-5.00, max_value=5.00, value=0.00, step=num_step)
     # モデルによる予測値（Predicted value）
     y_pred = p * x_input + q
     # 残差平方和（RSS）の計算
     rss = np.sum((y_input - y_pred) ** 2)
-with display_col02[0]:
+
+with display_col02[1]:
     # 残差平方和を表示
     st.write(f"現在の RSS（残差平方和）: **{rss:.2f}**")
-    if st.button("初期化") or ("history" not in st.session_state):
-        st.session_state.history = []  # 初期化 
-# 試行回数を最大100回に制限
-# if st.button("記録する"):
+
+
+# 試行回数を最大1000回に制限
 if len(st.session_state.history) >= 1000:
     st.session_state.history.pop(0)  # 最も古いデータを削除
+
 st.session_state.history.append({"p": p, "q": q, "RSS": rss})
 
 
 
 display_col03 = st.columns([1,1])
 
-
-with display_col03[0]:
+with display_col03[1]:
     # SessionState からデータ取得
     df_history = pd.DataFrame(st.session_state.history)
 
@@ -60,33 +63,9 @@ with display_col03[0]:
         )
     )
 
-    # # 3D 曲面を作成するための p, q 範囲を決定
-    # p_vals = np.linspace(df_history["p"].min(), df_history["p"].max(), 30)
-    # q_vals = np.linspace(df_history["q"].min(), df_history["q"].max(), 30)
-    # P, Q = np.meshgrid(p_vals, q_vals)
-
-    # # 残差平方和（RSS）の計算
-    # RSS = np.zeros_like(P)  # P と同じ形の 2D 配列を用意
-
-    # for i in range(P.shape[0]):
-    #     for j in range(P.shape[1]):
-    #         p_val = P[i, j]
-    #         q_val = Q[i, j]
-    #         RSS[i, j] = np.sum((y_input - (p_val * x_input + q_val)) ** 2)
-
-    # # 3D 等高線（曲面）を追加
-    # fig.add_trace(
-    #     go.Surface(
-    #         x=P, y=Q, z=RSS,
-    #         colorscale="Rainbow",
-    #         opacity=0.5,  # 半透明にして散布図を見えやすくする
-    #         showscale=False  # 色スケールをオフ
-    #     )
-    # )
-
     # Streamlit で表示
     st.plotly_chart(fig)
-with display_col03[1]:
+with display_col03[0]:
     # Figure 作成
     fig = go.Figure()
     # 散布図
@@ -107,7 +86,7 @@ with display_col04[0]:
     st.markdown(f"$${{\\rm RSS}}_{{\\rm min}} = {optimal_row[2]: .5f}$$")
     """ """
     st.markdown("#### RSSの最小値を与える線形回帰モデル")
-    st.markdown(f"$$y={optimal_row[0]: .5f}x+{optimal_row[1]: .5f}$$")
+    st.markdown(f"$$y={optimal_row[0]: .3f}x+{optimal_row[1]: .3f}$$")
 
 with display_col04[1]:
     # 履歴データの表示
